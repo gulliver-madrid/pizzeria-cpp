9167345b93437ce0bb633ab8490a4ee0ca510a87
gulliver-madrid <49131885+gulliver-madrid@users.noreply.github.com>
2023-09-12 01:47:49 +0200

    Crea demo_map

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,19 @@
 #include "juego.h"
+#include "pruebas/demo_map.h"
+#include <cassert>
+#include <map>
 
-int main() { //
-    return juego();
+#define APP 0
+
+int main() { // fmt
+    switch (APP) {
+    case 1:
+        return demo();
+        break;
+
+    default:
+        assert(APP == 0);
+        return juego();
+        break;
+    }
 }
---
+++ src/pruebas/demo_map.cpp
@@ -0,0 +1,27 @@
+#include "demo_map.h"
+
+#include <iostream>
+#include <map>
+
+int demo() { //
+    std::cout << "Demo map" << std::endl;
+    // Declara e inicializa un map que mapea strings a enteros
+    std::map<std::string, int> edad;
+
+    // Inserta algunos elementos
+    edad["Alice"] = 30;
+    edad["Bob"] = 40;
+    edad["Charlie"] = 50;
+
+    // Accede y muestra el valor usando una clave
+    std::cout << std::endl
+              << "La edad de Alice es " << edad["Alice"] << std::endl;
+
+    // Itera a través del map
+    for (auto &par : edad) {
+        std::cout << par.first << " tiene " << par.second << " años."
+                  << std::endl;
+    }
+
+    return 0;
+}
