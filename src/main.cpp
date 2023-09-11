6edbc185495323d4f64a84b4e2e5e36bd6759819
gulliver-madrid <49131885+gulliver-madrid@users.noreply.github.com>
2023-09-12 01:56:17 +0200

    El número de app se pasa por la línea de comandos

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,29 @@
 #include "demos/map.h"
 #include "juego.h"
 #include <cassert>
+#include <cstdlib> // Para std::atoi
+#include <iostream>
 
 #define APP 0
 
-int main() { // fmt
-    switch (APP) {
+int main(int argc, char *argv[]) { //
+    if (argc > 2) {
+        std::cout << "Uso: " << argv[0] << " <app>\n";
+        return 1;
+    }
+    int app;
+    if (argc == 2)
+        app = std::atoi(argv[1]);
+    else
+        app = APP;
+
+    switch (app) {
     case 1:
         return demo_map();
         break;
 
     default:
-        assert(APP == 0);
+        assert(app == 0);
         return juego();
         break;
     }
