98c6a309908999263496588e82f530798a888d57
gulliver-madrid <49131885+gulliver-madrid@users.noreply.github.com>
2023-10-17 10:02:00 +0200

    Extrae función y mejora manejo de errores

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,8 @@
 #include <cassert>
 #include <iostream>
 
+#define USER_ERROR 1
+
 enum class AppNombrada {
     Juego,
     Nivel,
@@ -29,27 +31,46 @@ const std::map<std::string, AppNombrada> cadena_to_app_nombrada = {
     {"map", AppNombrada::Map},         //
 };
 
-int main(int argc, char *argv[]) { //
-    std::cout << std::endl;
-    if (argc > 3) {
-        std::cout << "Demasiados argumentos" << std::endl;
-        return 1;
-    }
-
-    const auto primer_arg = argc > 1 ? argv[1] : "";
+AppNombrada convertir_a_app_nombrada(const char *const nombre_app) {
     AppNombrada app_nombrada;
-    if (cadena_to_app_nombrada.count(primer_arg)) {
-        app_nombrada = cadena_to_app_nombrada.at(primer_arg);
+    if (cadena_to_app_nombrada.count(nombre_app)) {
+        app_nombrada = cadena_to_app_nombrada.at(nombre_app);
     } else {
         app_nombrada = AppNombrada::Default;
     }
+    return app_nombrada;
+}
+
+int main(int argc, char *argv[]) {
+    std::cout << std::endl;
+    if (argc > 3) {
+        std::cout << "Demasiados argumentos" << std::endl;
+        return USER_ERROR;
+    }
+
+    const auto primer_arg = argc > 1 ? argv[1] : "";
+    const AppNombrada app_nombrada = convertir_a_app_nombrada(primer_arg);
     switch (app_nombrada) {
         case AppNombrada::Juego:
             return juego();
             break;
         case AppNombrada::Nivel:
             {
-                const NumNivel num_nivel(std::stoi(argv[2]));
+                int num_nivel_introducido;
+                try {
+                    num_nivel_introducido = std::stoi(argc > 2 ? argv[2] : "");
+                } catch (std::invalid_argument) {
+                    std::cout << "Debe introducirse un número de nivel (un "
+                                 "entero positivo)"
+                              << std::endl;
+                    return USER_ERROR;
+                }
+                if (num_nivel_introducido <= 0) {
+                    std::cout << "El número de nivel debe ser mayor que 0"
+                              << std::endl;
+                    return USER_ERROR;
+                }
+                NumNivel num_nivel(num_nivel_introducido);
                 return demo_nivel(num_nivel);
             }
             break;
