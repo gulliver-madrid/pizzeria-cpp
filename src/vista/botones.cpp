6d1099ab59ff9b06690ddced6e80868f56c0b0f6
gulliver-madrid <49131885+gulliver-madrid@users.noreply.github.com>
2023-10-17 11:28:13 +0200

    Refactoriza botones.cpp
    - Usa funciones en un namespace anónimo para generar los grupos de botones.

--- src/vista/botones.cpp
+++ src/vista/botones.cpp
@@ -5,9 +5,102 @@
 #include "componentes/varios.h"
 #include <cassert>
 
+///////////////////////////////////////////
+// BotonesGenerales
+//////////////////////////////////////////
+
 const std::vector<BotonConTexto *> BotonesGenerales::obtener_todos() {
     return {&alternar_grid, &reiniciar, &salir};
 }
+
+///////////////////////////////////////////
+// Botones
+//////////////////////////////////////////
+
+namespace {
+
+    const std::vector<BotonData> datos_botones_generales = {
+        {"Salir", sf::Color::Red},             //
+        {"Reiniciar", sf::Color(255, 120, 0)}, //
+        {"Alternar Grid", sf::Color::Blue}     //
+    };
+
+    void _crear_botones_encargar(
+        TipoPizzaToBoton &encargar,                //
+        const sf::Font &font,                      //
+        const modelo::TiposDePizza &tp_disponibles //
+    ) {
+        int i = 0;
+        for (auto tp : tp_disponibles) {
+            auto pos_panel = basicos_vista::obtener_posicion_panel(
+                IndicePanel::PANEL_ENCARGAR
+            );
+            BotonData encargar_tp_data{
+                tipo_pizza_to_string[tp], sf::Color::Green, sf::Color::Black
+            };
+            encargar[tp] = crearBotonConTexto(
+                encargar_tp_data,
+                sf::Vector2f(
+                    pos_panel.x + medidas::MARGEN_IZQ_ETIQUETAS,
+                    pos_panel.y + medidas::FILA_CONTENIDO_PANEL + 80 * i
+                ),
+                font
+            );
+            i++;
+        }
+    }
+    void _crear_botones_despachar(
+        TipoPizzaToBoton &despachar,               //
+        const sf::Font &font,                      //
+        const modelo::TiposDePizza &tp_disponibles //
+    ) {
+        const auto pos_x_boton_despachar_rel_panel =
+            medidas::MARGEN_IZQ_ETIQUETAS + (medidas::ANCHO_PANEL * 0.55);
+        const BotonData despachar_tp{
+            "Despachar", sf::Color::Green, sf::Color::Black
+        };
+        const int separacion_vertical_botones_despachar = 50;
+        const double escala_botones_despachar = 0.7;
+
+        size_t i = 0;
+        for (auto tp : tp_disponibles) {
+            auto pos_panel = basicos_vista::obtener_posicion_panel( //
+                IndicePanel::PANEL_PREPARADAS
+            );
+            const auto pos_x = pos_panel.x + pos_x_boton_despachar_rel_panel;
+            const auto pos_y = pos_panel.y + medidas::FILA_CONTENIDO_PANEL +
+                               (separacion_vertical_botones_despachar * i);
+            despachar[tp] = crearBotonConTexto(
+                despachar_tp, sf::Vector2f(pos_x, pos_y), font, Align::Left,
+                escala_botones_despachar
+            );
+            i++;
+        }
+    }
+
+    void _crear_botones_generales(
+        BotonesGenerales &generales, //
+        const sf::Font &font         //
+    ) {
+        const auto pos_ultimo_panel = basicos_vista::obtener_posicion_panel( //
+            IndicePanel::PANEL_PEDIDOS
+        );
+        const auto pos_dcha_ultimo_boton =
+            pos_ultimo_panel.x + medidas::ANCHO_PANEL;
+
+        auto botones_generales = crear_botones_alineados_derecha(
+            {pos_dcha_ultimo_boton, medidas::FILA_BOTONES_GENERALES}, //
+            datos_botones_generales,                                  //
+            font,                                                     //
+            medidas::SEPARACION_HORIZONTAL_ENTRE_BOTONES_GENERALES    //
+        );
+
+        assert(botones_generales.size() == 3);
+        generales.salir = botones_generales[0];
+        generales.reiniciar = botones_generales[1];
+        generales.alternar_grid = botones_generales[2];
+    }
+} // namespace
 
 /* Crea todos los botones */
 Botones::Botones(
@@ -16,64 +109,10 @@ Botones::Botones(
     auto empezar_data =
         BotonData{std::string("Empezar"), sf::Color::Green, sf::Color::Black};
     empezar = crearBotonConTexto(empezar_data, sf::Vector2f(500, 450), font);
-    int i = 0;
-    for (auto tp : tp_disponibles) {
-        auto pos_panel =
-            basicos_vista::obtener_posicion_panel(IndicePanel::PANEL_ENCARGAR);
-        BotonData encargar_tp_data{
-            tipo_pizza_to_string[tp], sf::Color::Green, sf::Color::Black
-        };
-        encargar[tp] = crearBotonConTexto(
-            encargar_tp_data,
-            sf::Vector2f(
-                pos_panel.x + medidas::MARGEN_IZQ_ETIQUETAS,
-                pos_panel.y + medidas::FILA_CONTENIDO_PANEL + 80 * i
-            ),
-            font
-        );
-        i++;
-    }
-    i = 0;
-    for (auto tp : tp_disponibles) {
-        auto pos_panel =
-            basicos_vista::obtener_posicion_panel(IndicePanel::PANEL_PREPARADAS
-            );
-        BotonData despachar_tp{"Despachar", sf::Color::Green, sf::Color::Black};
-        despachar[tp] = crearBotonConTexto(
-            despachar_tp,
-            sf::Vector2f(
-                pos_panel.x + medidas::MARGEN_IZQ_ETIQUETAS +
-                    (medidas::ANCHO_PANEL * 0.55),
-                pos_panel.y + medidas::FILA_CONTENIDO_PANEL + 50 * i
-            ),
-            font, Align::Left, 0.7
-        );
-        i++;
-    }
-    // Botones generales
-    const auto pos_ultimo_panel = basicos_vista::obtener_posicion_panel( //
-        IndicePanel::PANEL_PEDIDOS
-    );
-    const auto pos_dcha_ultimo_boton =
-        pos_ultimo_panel.x + medidas::ANCHO_PANEL;
 
-    const std::vector<BotonData> datos_botones = {
-        {"Salir", sf::Color::Red},             //
-        {"Reiniciar", sf::Color(255, 120, 0)}, //
-        {"Alternar Grid", sf::Color::Blue}     //
-    };
-
-    auto botones_generales = crear_botones_alineados_derecha(
-        {pos_dcha_ultimo_boton, medidas::FILA_BOTONES_GENERALES}, //
-        datos_botones,                                            //
-        font,                                                     //
-        medidas::SEPARACION_HORIZONTAL_ENTRE_BOTONES_GENERALES    //
-    );
-
-    assert(botones_generales.size() == 3);
-    generales.salir = botones_generales[0];
-    generales.reiniciar = botones_generales[1];
-    generales.alternar_grid = botones_generales[2];
+    _crear_botones_encargar(encargar, font, tp_disponibles);
+    _crear_botones_despachar(despachar, font, tp_disponibles);
+    _crear_botones_generales(generales, font);
 
     todos = {&empezar};
     const int num_fijos = todos.size();
