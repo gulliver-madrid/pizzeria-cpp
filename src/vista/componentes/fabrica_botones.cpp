e92b60510a27698ed8cdef56fad3ab430fe3bdb6
gulliver-madrid <49131885+gulliver-madrid@users.noreply.github.com>
2023-10-18 18:20:36 +0200

    Extrae funciones de crear_botones...derecha

--- src/vista/componentes/fabrica_botones.cpp
+++ src/vista/componentes/fabrica_botones.cpp
@@ -3,6 +3,40 @@
 #include <cassert>
 
 /*
+ * Crea una hilera de botones.
+ */
+std::vector<BotonConTexto> crear_botones(
+    const std::vector<BotonData> &datos_botones, //
+    const sf::Font &font
+) {
+    // Crea los botones
+    std::vector<BotonConTexto> botones;
+    for (auto &dato : datos_botones) {
+        botones.emplace_back(dato, font);
+    }
+    return botones;
+}
+
+/*
+ * Alinea una hilera de botones a la derecha. El orden en el que se
+ * reciben los datos y se anaden al vector de botones es de derecha a izquierda.
+ */
+void alinear_botones_derecha(
+    std::vector<BotonConTexto *> botones,
+    const sf::Vector2f &posicion_inicial, //
+    int separacion
+) {
+    // Posiciona los botones
+    int next_pos_x = posicion_inicial.x;
+    for (auto &boton : botones) {
+        const auto posicion = sf::Vector2f(next_pos_x, posicion_inicial.y);
+        boton->establecerPosicion(posicion, Align::Right);
+        const auto anterior_izquierda = boton->getGlobalBounds().left;
+        next_pos_x = anterior_izquierda - separacion;
+    }
+}
+
+/*
  * Crea una hilera de botones alienados a la derecha. El orden en el que se
  * reciben los datos y se anaden al vector de botones es de derecha a izquierda.
  */
@@ -12,18 +46,11 @@ std::vector<BotonConTexto> crear_botones_alineados_derecha(
     const sf::Font &font,                        //
     int separacion
 ) {
-    // Crea los botones
-    std::vector<BotonConTexto> botones;
-    for (auto &dato : datos_botones) {
-        botones.emplace_back(dato, font);
-    }
-    // Posiciona los botones
-    int next_pos_x = posicion_inicial.x;
+    auto botones = crear_botones(datos_botones, font);
+    std::vector<BotonConTexto *> vect_ptr;
     for (auto &boton : botones) {
-        const auto posicion = sf::Vector2f(next_pos_x, posicion_inicial.y);
-        boton.establecerPosicion(posicion, Align::Right);
-        const auto anterior_izquierda = boton.getGlobalBounds().left;
-        next_pos_x = anterior_izquierda - separacion;
+        vect_ptr.push_back(&boton);
     }
+    alinear_botones_derecha(vect_ptr, posicion_inicial, separacion);
     return botones;
 }
