59a425ace641bbdcb803a44943d8f6bf99d8b062
gulliver-madrid <49131885+gulliver-madrid@users.noreply.github.com>
2023-10-26 19:26:32 +0200

    Simplifica comandos.h eliminando TipoBoton

--- src/comandos.h
+++ src/comandos.h
@@ -3,36 +3,17 @@
 #include "modelo/dominio.h"
 #include <variant>
 
-enum class TipoBoton {
-    Salir,
-    Reiniciar,
-    AlternarGrid,
-    Empezar,
-    Encargar,
-    Despachar
-};
-
 class Comando {
   public:
-    struct Empezar {
-        static const TipoBoton tipo() { return TipoBoton::Empezar; }
-    };
-    struct Salir {
-        static const TipoBoton tipo() { return TipoBoton::Salir; }
-    };
-    struct AlternarGrid {
-        static const TipoBoton tipo() { return TipoBoton::AlternarGrid; }
-    };
-    struct Reiniciar {
-        static const TipoBoton tipo() { return TipoBoton::Reiniciar; }
-    };
+    struct Empezar {};
+    struct Salir {};
+    struct AlternarGrid {};
+    struct Reiniciar {};
     struct Encargar {
         modelo::TipoPizza tp;
-        static const TipoBoton tipo() { return TipoBoton::Encargar; }
     };
     struct Despachar {
         modelo::TipoPizza tp;
-        static const TipoBoton tipo() { return TipoBoton::Despachar; }
     };
 
     using ComandoVar = std::variant<
