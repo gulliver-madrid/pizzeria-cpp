7913c3998f20a8fd60c6e780f052f0af59608150
gulliver-madrid <49131885+gulliver-madrid@users.noreply.github.com>
2024-01-02 20:59:41 +0100

    ModeloAmplio tiene método aplica_comando()

--- src/modelo_amplio.h
+++ src/modelo_amplio.h
@@ -4,10 +4,17 @@
 #include "general.h"
 
 struct DatosNivel;
+struct Comando;
 
 struct ModeloAmplio {
     Estado estado;
     FaseNivel get_fase_actual() { return estado.fase_actual; }
     ModeloAmplio(const DatosNivel &datos_nivel)
         : estado(FaseNivel::MostrandoInstrucciones, datos_nivel) {}
+
+    FaseNivel aplica_comando(  //
+        const Comando &comando //
+    ) {
+        return FaseNivel::Saliendo;
+    }
 };
--- src/tests/modelo/test_modelo_amplio.cpp
+++ src/tests/modelo/test_modelo_amplio.cpp
@@ -1,4 +1,5 @@
 
+#include "../../comandos.h"
 #include "../../datos_nivel.h"
 #include "../../modelo_amplio.h"
 #include <cassert>
@@ -13,4 +14,11 @@ TEST(ModeloAmplio, ModeloAmplioEmpiezaMostrandoInstrucciones) {
     EXPECT_EQ(
         modelo_amplio.get_fase_actual(), FaseNivel::MostrandoInstrucciones
     );
+}
+TEST(ModeloAmplio, ModeloAmplioAplicaComandoSalir) {
+    const std::string instrucciones = "test instrucciones";
+    const DatosNivel datos_nivel;
+    ModeloAmplio modelo_amplio(datos_nivel);
+    const auto result = modelo_amplio.aplica_comando(Comando::Salir{});
+    EXPECT_EQ(result, FaseNivel::Saliendo);
 }
