b9dd9abb2926bf97f6e6c7405ba6011ec2900bc4
gulliver-madrid <49131885+gulliver-madrid@users.noreply.github.com>
2024-01-06 18:35:16 +0100

    Añade modo dinamico y log to file
    - Funciona porque he agregado a la path las .dll porque necesita encontrarlas en runtime, y ahi el cmakelists ya no pinta nada.

--- src/shared.cpp
+++ src/shared.cpp
@@ -1,11 +1,16 @@
 #include "shared.h"
 #include <boost/log/core.hpp>
 #include <boost/log/expressions.hpp>
+#include <boost/log/utility/setup/file.hpp>
+
+const std::string LOG_FILENAME = "output/runtime.log";
 
 namespace logging = boost::log;
 namespace expr = boost::log::expressions;
 
 void init_log() {
+    logging::add_file_log(LOG_FILENAME);
+
     logging::core::get()->set_filter(
         logging::trivial::severity >= logging::trivial::info
     );
