69994fab5d5ce850a466575478b070d2de8d9223
gulliver-madrid <49131885+gulliver-madrid@users.noreply.github.com>
2024-01-09 14:11:34 +0100

    Create several log sinks

--- src/shared.cpp
+++ src/shared.cpp
@@ -2,23 +2,46 @@
 #include <boost/log/core.hpp>
 #include <boost/log/expressions.hpp>
 #include <boost/log/utility/setup/common_attributes.hpp>
+#include <boost/log/utility/setup/console.hpp>
 #include <boost/log/utility/setup/file.hpp>
-
-const std::string LOG_FILENAME = "output/runtime.log";
+#include <filesystem>
 
 namespace logging = boost::log;
 namespace expr = boost::log::expressions;
 namespace keywords = boost::log::keywords;
 
 void init_log() {
-    logging::add_common_attributes(); // anade data como TimeStamp
+    auto core = logging::core::get();
+    core->set_logging_enabled(_DEBUG);
+    std::filesystem::path logFileFolder = std::filesystem::current_path();
+    logFileFolder.append("logs");
+    auto info_file = logFileFolder / "log_info_%N.log";
+    auto debug_file = logFileFolder / "log_debug_%N.log";
+    // Sink para mensajes de info y superiores
     logging::add_file_log(
-        keywords::file_name = LOG_FILENAME,
-        keywords::rotation_size = 10 * 1024 * 1024, // 10Mb
+        keywords::file_name = info_file,
+        keywords::rotation_size = 100 * 1024, // Rotacion cada 100Kb
+        keywords::max_files = 8,              //
+        keywords::target = logFileFolder,
+        keywords::filter = logging::trivial::severity >= logging::trivial::info,
+        keywords::format = "[%TimeStamp%] [%Severity%]: %Message%"
+    );
+
+    // Sink para mensajes de debug
+    logging::add_file_log(
+        keywords::file_name = debug_file,
+        keywords::rotation_size = 10 * 1024 * 1024,
+        keywords::filter =
+            logging::trivial::severity == logging::trivial::debug,
         keywords::format = "[%TimeStamp%]: %Message%"
     );
 
-    logging::core::get()->set_filter(
-        logging::trivial::severity >= logging::trivial::info
+    logging::add_console_log(
+        std::cout, keywords::format = "[%TimeStamp%] [%Severity%]: %Message%",
+        keywords::filter =
+            logging::trivial::severity >= logging::trivial::warning
     );
+
+    // Atributos comunes
+    logging::add_common_attributes();
 }
