abd9b43132ee237c6f06cbc07f8b927ae577ebe6
gulliver-madrid <49131885+gulliver-madrid@users.noreply.github.com>
2024-01-10 10:59:16 +0100

    Use test fixture for test_activacion_botones

--- src/tests/presentador/test_activacion_botones.cpp
+++ src/tests/presentador/test_activacion_botones.cpp
@@ -11,6 +11,9 @@ using dominio::TipoPizza;
 using modelo::Pedidos;
 using PizzaToBool = std::map<TipoPizza, bool>;
 
+// Estos tests chequean la creacion correcta de ActivacionBotones
+// en varios escenarios
+
 // Crea un modelo interno a partir de un conjunto de pedidos
 ModeloInterno crear_modelo_interno(modelo::Pedidos pedidos) {
     DatosModeloInterno datos_modelo_interno(pedidos);
@@ -21,64 +24,91 @@ Pedidos crear_pedidos_margarita(unsigned int num_pizzas) {
     return {Pedido({{TipoPizza::Margarita, num_pizzas}})};
 }
 
-TEST(Presentador, ActivacionBotonesCorrectaSiPedidosEstaVacio) {
-    ModeloInterno modelo = crear_modelo_interno({});
-    const auto activacion = obtener_activacion_botones(modelo);
+///// ActivacionTest /////
+
+class ActivacionTest : public testing::Test {
+  private:
+    ActivacionBotones _obtener_activacion() {
+        return obtener_activacion_botones(_modelo.value());
+    }
+
+  protected:
+    std::optional<ModeloInterno> _modelo;
+    void _crear_modelo(Pedidos pedidos) {
+        _modelo.emplace(crear_modelo_interno(pedidos));
+    }
+
+    void _assert_activacion_encargar(bool expected) {
+        auto activacion = _obtener_activacion();
+        ASSERT_EQ(activacion.encargar, expected);
+    }
+    void _assert_despachar_eq(PizzaToBool expected) {
+        auto activacion = _obtener_activacion();
+        ASSERT_EQ(activacion.despachar, expected);
+    }
+};
+
+TEST_F(ActivacionTest, ActivacionBotonesCorrectaSiPedidosEstaVacio) {
+    _crear_modelo({});
 
     // No debe haber botones despachar al no haber pedidos
     // que hagan referencia a ningun tipo de pizza
-    ASSERT_TRUE(activacion.despachar.empty());
+    _assert_despachar_eq({});
 
     // Cualquier boton encargar estaria activo al no haber
     // demasiadas pizzas en preparacion
-    ASSERT_EQ(activacion.encargar, true);
+    _assert_activacion_encargar(true);
 }
 
-TEST(
-    Presentador, BotonesDespacharDesactivadosSiHayDemasiadosPedidosSimultaneos
+TEST_F(
+    ActivacionTest,
+    BotonesDespacharDesactivadosSiHayDemasiadosPedidosSimultaneos
 ) {
-    Pedidos pedidos = crear_pedidos_margarita(4);
-    ModeloInterno modelo = crear_modelo_interno(pedidos);
-
-    auto activacion = obtener_activacion_botones(modelo);
-    // hay un boton despachar desactivado
-    PizzaToBool expected = {{TipoPizza::Margarita, false}};
-    ASSERT_EQ(activacion.despachar, expected);
+    const auto maximo = modelo_info::MAXIMO_PIZZAS_EN_PREPARACION;
+    Pedidos pedidos = crear_pedidos_margarita(maximo + 1);
+    _crear_modelo(pedidos);
 
     // Verifica el estado de activacion de los botones encargar
-    assert(modelo_info::MAXIMO_PIZZAS_EN_PREPARACION == 3);
-    for (int i = 0; i < 3; i++) {
+    for (int i = 0; i < maximo; ++i) {
         // Mientras no llega al maximo el boton estara activado
-        activacion = obtener_activacion_botones(modelo);
-        ASSERT_EQ(activacion.encargar, true);
-        modelo.anadir_encargo(TipoPizza::Margarita);
+        _assert_activacion_encargar(true);
+        _modelo.value().anadir_encargo(TipoPizza::Margarita);
     }
 
     // Tras encargar la ultima no es posible encargar mas
-    activacion = obtener_activacion_botones(modelo);
-    ASSERT_EQ(activacion.encargar, false);
+    _assert_activacion_encargar(false);
 }
 
-TEST(Presentador, BotonDespacharDesctivadoConPizzaNoPreparada) {
-    Pedidos pedidos = crear_pedidos_margarita(2);
-    ModeloInterno modelo = crear_modelo_interno(pedidos);
+///// ActivacionConPedidoDosMargaritasTest /////
 
-    auto activacion = obtener_activacion_botones(modelo);
-    // hay un boton despachar desactivado
-    PizzaToBool expected = {{TipoPizza::Margarita, false}};
-    ASSERT_EQ(activacion.despachar, expected);
+class ActivacionConPedidoDosMargaritasTest : public ActivacionTest {
+  protected:
+    void SetUp() override {
+        Pedidos pedidos = crear_pedidos_margarita(2);
+        _crear_modelo(pedidos);
+    }
+    void preparar_pizza() {
+        // Encarga una pizza y pasa el tiempo para prepararla
+        assert(_modelo.has_value());
+        auto &modelo = _modelo.value();
+        modelo.gestor_tiempo.activar();
+        modelo.anadir_encargo(TipoPizza::Margarita);
+        modelo.gestor_tiempo.tick(sf::seconds(10));
+        modelo.evaluar_preparacion_pizzas();
+    }
+};
+
+TEST_F(
+    ActivacionConPedidoDosMargaritasTest,
+    BotonDespacharDesactivadoConPizzaNoPreparada
+) {
+    _assert_despachar_eq({{TipoPizza::Margarita, false}});
 }
 
-TEST(Presentador, UnBotonDespacharActivadoSiHayUnaPizzaPreparada) {
-    Pedidos pedidos = crear_pedidos_margarita(2);
-    ModeloInterno modelo = crear_modelo_interno(pedidos);
-
-    modelo.gestor_tiempo.activar();
-    modelo.anadir_encargo(TipoPizza::Margarita);
-    modelo.gestor_tiempo.tick(sf::seconds(10));
-    modelo.evaluar_preparacion_pizzas();
-
-    auto activacion = obtener_activacion_botones(modelo);
-    PizzaToBool expected = {{TipoPizza::Margarita, true}};
-    ASSERT_EQ(activacion.despachar, expected);
+TEST_F(
+    ActivacionConPedidoDosMargaritasTest,
+    BotonDespacharActivadoSiHayUnaPizzaPreparada
+) {
+    preparar_pizza();
+    _assert_despachar_eq({{TipoPizza::Margarita, true}});
 }
