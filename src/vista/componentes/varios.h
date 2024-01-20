85d98954cdbf3358bd3bab7d6078099e8e3ebf04
gulliver-madrid <49131885+gulliver-madrid@users.noreply.github.com>
2024-01-20 14:08:05 +0100

    Remove old crearEtiqueta functions

--- src/vista/componentes/varios.h
+++ src/vista/componentes/varios.h
@@ -8,18 +8,17 @@
 #include <optional>
 #include <string>
 
+class Etiqueta;
+struct EstiloTexto;
+
 namespace sf {
     class Font;
     class Text;
 } // namespace sf
 
-class Etiqueta;
-
 namespace medidas {
     constexpr int TAMANO_TEXTO_GRANDE = 32;
 } // namespace medidas
-
-struct EstiloTexto;
 
 const auto Vector2f_CERO = sf::Vector2f(0, 0);
 
@@ -46,27 +45,4 @@ std::shared_ptr<Etiqueta> crear_etiqueta(
     const EstiloTexto &estilo,   //
     const OptionalFont &font,    //
     const sf::Vector2f &posicion //
-);
-
-///// Creadores de sf::Text /////
-
-sf::Text crearEtiqueta(
-    const std::string &texto, //
-    int tamano_fuente,        //
-    const OptionalFont &      //
-);
-
-sf::Text crearEtiqueta(
-    const std::string &texto,                    //
-    int tamano_fuente,                           //
-    const sf::Color &,                           //
-    const OptionalFont &,                        //
-    const sf::Vector2f &posicion = Vector2f_CERO //
-
-);
-sf::Text crearEtiqueta(
-    const std::string &texto,                    //
-    const EstiloTexto &,                         //
-    const OptionalFont &,                        //
-    const sf::Vector2f &posicion = Vector2f_CERO //
 );
