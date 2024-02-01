229e851d84eb4dbbbddad998d8a8c75c03ae1428
gulliver-madrid <49131885+gulliver-madrid@users.noreply.github.com>
2024-02-01 10:10:53 +0100

    Recupera estado previo de manejo_rutas.h

--- src/manejo_rutas.h
+++ src/manejo_rutas.h
@@ -1,48 +1,47 @@
 #pragma once
 
 #include <filesystem>
-#include <iostream>
 #include <string>
-
+#ifdef _WIN32
 // Siempre hay que definir NOMINMAX antes de incluir windows.h
 #define NOMINMAX
 #include <cstdlib>
 #include <windows.h>
 
 std::string narrow(const std::wstring &w_cadena) {
-    size_t tamano = 0;
-    // Primero obtenemos el tamaño necesario para el buffer.
-    wcstombs_s(&tamano, nullptr, 0, w_cadena.c_str(), _TRUNCATE);
+    auto tamano = std::wcstombs(nullptr, w_cadena.c_str(), 0);
     std::string cadena(tamano, '\0');
-    // Luego convertimos la cadena.
-    wcstombs_s(&tamano, &cadena[0], tamano, w_cadena.c_str(), _TRUNCATE);
+    std::wcstombs(&cadena[0], w_cadena.c_str(), tamano);
     return cadena;
 };
-
-// std::string narrow(const std::wstring &w_cadena) {
-//     auto tamano = std::wcstombs(nullptr, w_cadena.c_str(), 0);
-//     std::string cadena(tamano, '\0');
-//     std::wcstombs(&cadena[0], w_cadena.c_str(), tamano);
-//     return cadena;
-// };
+#elif __linux__
+#include <limits.h>
+#include <unistd.h>
+#endif
 
 std::string obtener_ruta_ejecutable() {
+#ifdef _WIN32
+
     wchar_t buffer[MAX_PATH];
     GetModuleFileNameW(NULL, buffer, MAX_PATH);
     std::wstring::size_type pos = std::wstring(buffer).find_last_of(L"\\/");
     return narrow(std::wstring(buffer).substr(0, pos));
+#elif __linux__
+    // TODO: test it in linux
+    char buffer[PATH_MAX];
+    ssize_t len = readlink("/proc/self/exe", buffer, sizeof(buffer) - 1);
+    if (len != -1) {
+        buffer[len] = '\0';
+        std::string::size_type pos = std::string(buffer).find_last_of("\\/");
+        return std::string(buffer).substr(0, pos);
+    }
+    return "";
+#endif
 }
 
 std::filesystem::path obtener_ruta_recurso(const std::string &ruta_relativa) {
     std::string ruta_ejecutable = obtener_ruta_ejecutable();
-    std::cout << "Ruta del ejecutable: " << ruta_ejecutable << std::endl;
     std::filesystem::path ruta_recurso(ruta_ejecutable);
-    ruta_recurso /= ruta_relativa; // Combina las rutas correctamente
-    std::filesystem::path ruta_absoluta =
-        std::filesystem::absolute(ruta_recurso);
-
-    // Imprimir la ruta para diagnóstico
-    std::cout << "Ruta del recurso: " << ruta_absoluta << std::endl;
-
-    return ruta_absoluta;
+    ruta_recurso /= ruta_relativa;
+    return ruta_recurso;
 }
