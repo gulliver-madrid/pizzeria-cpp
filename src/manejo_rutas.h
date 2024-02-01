b8a8afbd1342ccf731e6f924a7e1339ad90c512f
gulliver-madrid <49131885+gulliver-madrid@users.noreply.github.com>
2024-02-01 01:07:37 +0100

    Intenta usar wcstombs_s, no se ejecuta
    Intenta usar wcstombs_s en manejo_rutas.h para evitar warning. Compila,
    pero al tratar de ejecutar aparece el error:
    Failed to load font "...\pizzeria\build\bin\Debug\./resources/fonts/main_font.ttf" (failed to create the font face)

--- src/manejo_rutas.h
+++ src/manejo_rutas.h
@@ -2,41 +2,34 @@
 
 #include <filesystem>
 #include <string>
-#ifdef _WIN32
+
 // Siempre hay que definir NOMINMAX antes de incluir windows.h
 #define NOMINMAX
 #include <cstdlib>
 #include <windows.h>
 
 std::string narrow(const std::wstring &w_cadena) {
-    auto tamano = std::wcstombs(nullptr, w_cadena.c_str(), 0);
+    size_t tamano = 0;
+    // Primero obtenemos el tamaño necesario para el buffer.
+    wcstombs_s(&tamano, nullptr, 0, w_cadena.c_str(), _TRUNCATE);
     std::string cadena(tamano, '\0');
-    std::wcstombs(&cadena[0], w_cadena.c_str(), tamano);
+    // Luego convertimos la cadena.
+    wcstombs_s(&tamano, &cadena[0], tamano, w_cadena.c_str(), _TRUNCATE);
     return cadena;
 };
-#elif __linux__
-#include <limits.h>
-#include <unistd.h>
-#endif
+
+// std::string narrow(const std::wstring &w_cadena) {
+//     auto tamano = std::wcstombs(nullptr, w_cadena.c_str(), 0);
+//     std::string cadena(tamano, '\0');
+//     std::wcstombs(&cadena[0], w_cadena.c_str(), tamano);
+//     return cadena;
+// };
 
 std::string obtener_ruta_ejecutable() {
-#ifdef _WIN32
-
     wchar_t buffer[MAX_PATH];
     GetModuleFileNameW(NULL, buffer, MAX_PATH);
     std::wstring::size_type pos = std::wstring(buffer).find_last_of(L"\\/");
     return narrow(std::wstring(buffer).substr(0, pos));
-#elif __linux__
-    // TODO: test it in linux
-    char buffer[PATH_MAX];
-    ssize_t len = readlink("/proc/self/exe", buffer, sizeof(buffer) - 1);
-    if (len != -1) {
-        buffer[len] = '\0';
-        std::string::size_type pos = std::string(buffer).find_last_of("\\/");
-        return std::string(buffer).substr(0, pos);
-    }
-    return "";
-#endif
 }
 
 std::filesystem::path obtener_ruta_recurso(const std::string &ruta_relativa) {
