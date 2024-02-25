429457300b78e8e9358a9b9fe79782dfefaefe37
gulliver-madrid <49131885+gulliver-madrid@users.noreply.github.com>
2024-02-25 14:01:47 +0100

    Refactor test_vista_pedido.cpp

--- src/tests/vistas/test_vista_pedido.cpp
+++ src/tests/vistas/test_vista_pedido.cpp
@@ -1,50 +1,44 @@
 #include "../../enlace_vista/presentador.h"
 #include "../../modelo/dominio.h"
 #include <gtest/gtest.h>
+#include <map>
 
 using dominio::TipoPizza;
 using ContenidoPedido = std::map<TipoPizza, LineaPedido>;
+using Pair = std::pair<int, int>;
+
+Pedido crear_pedido_prueba(Pair margarita, Pair pepperoni) {
+    ContenidoPedido contenido;
+    contenido[TipoPizza::Margarita] =
+        LineaPedido{margarita.first, margarita.second};
+    contenido[TipoPizza::Pepperoni] =
+        LineaPedido{pepperoni.first, pepperoni.second};
+
+    Pedido pedido(std::move(contenido));
+    return pedido;
+}
 
 TEST(VistaPedido, NumLineas) {
-    ContenidoPedido contenido(
-        {{TipoPizza::Margarita, {2, 5}}, //
-         {TipoPizza::Pepperoni, {1, 4}}}
-    );
-    Pedido pedido(std::move(contenido));
+    const auto pedido = crear_pedido_prueba({2, 5}, {1, 4});
     const auto vista_pedido = presentador::impl::pedido_to_vista(pedido);
     auto result = vista_pedido.get_num_lineas();
     ASSERT_EQ(2, result);
 }
 
-TEST(VistaPedido, NoEstaCubiertoSiNingunaLineaEstaCubierta) {
-    ContenidoPedido contenido(
-        {{TipoPizza::Margarita, {2, 5}}, //
-         {TipoPizza::Pepperoni, {1, 4}}}
-    );
-    Pedido pedido(std::move(contenido));
+void comprobar_caso(Pair margarita, Pair pepperoni, bool estaCubiertoEsperado) {
+    const auto pedido = crear_pedido_prueba(margarita, pepperoni);
     const auto vista_pedido = presentador::impl::pedido_to_vista(pedido);
-    auto result = vista_pedido.esta_cubierto();
-    ASSERT_EQ(false, result);
+    ASSERT_EQ(estaCubiertoEsperado, vista_pedido.esta_cubierto());
+}
+
+TEST(VistaPedido, NoEstaCubiertoSiNingunaLineaEstaCubierta) {
+    comprobar_caso({2, 5}, {1, 4}, false);
 }
 
 TEST(VistaPedido, NoEstaCubiertoAunqueHayaLineasCubiertas) {
-    ContenidoPedido contenido(
-        {{TipoPizza::Margarita, {5, 5}}, //
-         {TipoPizza::Pepperoni, {1, 4}}}
-    );
-    Pedido pedido(std::move(contenido));
-    const auto vista_pedido = presentador::impl::pedido_to_vista(pedido);
-    auto result = vista_pedido.esta_cubierto();
-    ASSERT_EQ(false, result);
+    comprobar_caso({5, 5}, {1, 4}, false);
 }
 
-TEST(VistaPedido, EstaCubierto) {
-    ContenidoPedido contenido(
-        {{TipoPizza::Margarita, {5, 5}}, //
-         {TipoPizza::Pepperoni, {4, 4}}}
-    );
-    Pedido pedido(std::move(contenido));
-    const auto vista_pedido = presentador::impl::pedido_to_vista(pedido);
-    auto result = vista_pedido.esta_cubierto();
-    ASSERT_EQ(true, result);
+TEST(VistaPedido, EstaCubierto) { //
+    comprobar_caso({5, 5}, {4, 4}, true);
 }
